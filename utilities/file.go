9f0103c8bc9fd7fb0ceea2f5ee9b95492d959159
Amin Ataei <maminataee@gmail.com>
2024-08-30 22:47:43 +0330

    file utility reimplemented

---
+++ utilities/file.go
@@ -0,0 +1,24 @@
+package utilities
+
+import (
+	"os"
+)
+
+type File struct {}
+
+func (j File) Save(path string, data string) error {
+	dataInByte := []byte(data)
+	err := os.WriteFile(path, dataInByte, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (j File) Read(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
