2be39a377becabb231a9cc4b4ea3ecff7b6a43f1
Amin Ataei <maminataee@gmail.com>
2024-08-30 22:49:20 +0330

    user repo implemented

--- repository/User.go
+++ repository/User.go
@@ -4,70 +4,76 @@ import (
 	"encoding/json"
 	"errors"
 	"todo/model"
-	"todo/utils"
+	"todo/utilities"
 )
 
 type UserRepository struct {
-	filePath string
+	FilePath string
+	FileUtil utilities.File
+	loggedIn bool
 }
-
-var file utils.File = utils.File{}
-
 func NewUserRepository(path string) UserRepository {
-	return UserRepository{filePath: path}
+	return UserRepository{FilePath: path, FileUtil: utilities.File{}, loggedIn: false}
 }
-
-func (repo UserRepository) CreateUser(user model.User) error {
-	usersStr, readJSONFileErr := file.Read(repo.filePath)
+func (repo *UserRepository) CreateUser(user model.User) error {
+	usersStr, readJSONFileErr := repo.FileUtil.Read(repo.FilePath)
 	if(readJSONFileErr != nil) {
-		return errors.New("Error reading file")
+		return errors.New("error reading file")
 	}
 	var users []model.User
 	jsonUnmarshalErr := json.Unmarshal([]byte(usersStr), &users)
 	if(jsonUnmarshalErr != nil) {
-		return errors.New("Error unmarshalling")
+		return errors.New("error unmarshalling")
 	}
 	users = append(users, user)
 	usersJson, usersJSONErr := json.Marshal(users)
 	if(usersJSONErr != nil) {
-		return errors.New("Error marshalling")
+		return errors.New("error marshalling")
 	}
-	saveJSONErr := file.Save(repo.filePath, string(usersJson))
+	saveJSONErr := repo.FileUtil.Save(repo.FilePath, string(usersJson))
 	if(saveJSONErr != nil) {
-		return errors.New("Error saving file")
+		return errors.New("error saving file")
 	}
 	return nil
 }
-
-func (repo UserRepository) ListAllUsers() ([]model.User, error) {
-	usersStr, readJSONFileErr := file.Read(repo.filePath)
+func (repo *UserRepository) ListAllUsers() ([]model.User, error) {
+	usersStr, readJSONFileErr := repo.FileUtil.Read(repo.FilePath)
 	if readJSONFileErr != nil {
-		return []model.User{}, errors.New("Error reading file")
+		return []model.User{}, errors.New("error reading file")
 	}
 	var users []model.User
 	jsonUnmarshalErr := json.Unmarshal([]byte(usersStr), &users)
 	if jsonUnmarshalErr != nil {
-		return []model.User{}, errors.New("Error reading file")
+		return []model.User{}, errors.New("error reading file")
 	}
 	return users, nil
 }
 
-func (repo UserRepository) GetUser(id int) (model.User, error) {
+func (repo *UserRepository) GetUser(id int) (model.User, error) {
 	users, err := repo.ListAllUsers()
 	if err != nil {
 		return model.User{}, err
 	}
-	findUser := model.User{}
 	for _, user := range users {
 		if user.Id == id {
-			findUser = user
-			return findUser, nil
+			return user, nil
 		}
 	}
 	return model.User{}, nil
 }
-
-func (repo UserRepository) EditUser(cat model.User) error {
+func (repo *UserRepository) GetUserByEmailAndPassword(email string, password string) (model.User, error) {
+	users, err := repo.ListAllUsers()
+	if err != nil {
+		return model.User{}, err
+	}
+	for _, user := range users {
+		if user.Email == email && user.Password == password {
+			return user, nil
+		}
+	}
+	return model.User{}, nil
+}
+func (repo *UserRepository) EditUser(cat model.User) error {
 	users, err := repo.ListAllUsers()
 	if err != nil {
 		return err
@@ -81,18 +87,18 @@ func (repo UserRepository) EditUser(cat model.User) error {
 			}
 			usersJson, usersJSONErr := json.Marshal(users)
 			if(usersJSONErr != nil) {
-				return errors.New("Error marshalling")
+				return errors.New("error marshalling")
 			}
-			saveJSONErr := file.Save(repo.filePath, string(usersJson))
+			saveJSONErr := repo.FileUtil.Save(repo.FilePath, string(usersJson))
 			if(saveJSONErr != nil) {
-				return errors.New("Error saving file")
+				return errors.New("error saving file")
 			}
 			return nil
 		}
 	}
-	return errors.New("User not found")
+	return errors.New("user not found")
 }
-func (repo UserRepository) DeleteUser(id int) error {
+func (repo *UserRepository) DeleteUser(id int) error {
 	users, err := repo.ListAllUsers()
 	if err != nil {
 		return err
@@ -102,14 +108,29 @@ func (repo UserRepository) DeleteUser(id int) error {
 			users = append(users[:i], users[i+1:]...)
 			usersJson, usersJSONErr := json.Marshal(users)
 			if(usersJSONErr != nil) {
-				return errors.New("Error marshalling")
+				return errors.New("error marshalling")
 			}
-			saveJSONErr := file.Save(repo.filePath, string(usersJson))
+			saveJSONErr := repo.FileUtil.Save(repo.FilePath, string(usersJson))
 			if(saveJSONErr != nil) {
-				return errors.New("Error saving file")
+				return errors.New("error saving file")
 			}
 			return nil
 		}
 	}
-	return errors.New("User not found")
+	return errors.New("user not found")
+}
+func (repo *UserRepository) LoginUser(email string, password string) error {
+	_, err := repo.GetUserByEmailAndPassword(email, password)
+	if err != nil {
+		repo.loggedIn = false
+		return err
+	}
+	repo.loggedIn = true
+	return nil
+}
+func (repo *UserRepository) LogoutUser() {
+	repo.loggedIn = false
+}
+func (repo UserRepository) IsLoggedIn() bool {
+	return repo.loggedIn
 }
